a2a56020486d2421ba3f931fc94ffcbf01d75647
marcin <marcin@vaadin.com>
2024-12-23 08:47:54 +0100

    comments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void       main(String[] args) {
+    // Example has 3 spaces between "void" and "main"
+    // It has been committed without formatting on save
+
+    // After cloning repo make sure you have formatting on save enabled
+    // 1. Add one more space and save -> formatted to single space
+    // 2. Add again 2 more spaces, to have same as in the beginning and save -> no formatting this time
+    public static void   main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
